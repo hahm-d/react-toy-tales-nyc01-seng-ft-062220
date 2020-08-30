0694d06fad6a7fcc463c3b66f7655a82bacdc32c
hahm-d <dhahm01@gmail.com>
2020-08-30 18:24:29 -0400

    Done.

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,12 +1,89 @@
 import React from 'react';
 import ToyCard from './ToyCard'
+import ToyForm from './ToyForm'
 
-const ToyContainer = () => {
-  return(
-    <div id="toy-collection">
-      {/* Render the collection of ToyCards */}
+class ToyContainer extends React.Component{
+//create an empty state of data which will be updated based on life cycle
+  state = {
+    data: [],
+    display: false
+  }
+
+  handleClick = () => {
+    let newBoolean = !this.state.display
+    this.setState({
+      display: newBoolean
+    })
+  }
+
+//after this container is mounted lets fetch the initial api
+  componentDidMount(){
+    fetch('http://localhost:3000/toys')
+    .then(resp => resp.json())
+    .then(data => this.setState({data: data}))
+  }
+
+  update() {
+    fetch('http://localhost:3000/toys')
+    .then(res => res.json())
+    .then(data => this.setState({data: data}))
+  }
+  
+  donateHandler = (id) => {
+    fetch(`http://localhost:3000/toys/${id}`, { method: 'DELETE'})
+    this.update()
+  }
+
+  likeHandler = (toyObj) => {
+    toyObj.likes += 1 
+    let settings = {
+      method: 'PATCH',
+      headers: {
+        'content-type': 'application/json',
+        'accept': 'application/json'
+      },
+      body: JSON.stringify({likes: parseInt(toyObj.likes)})
+    }
+    fetch(`http://localhost:3000/toys/${toyObj.id}`, settings)
+    .then(resp => resp.json())
+    .then(toyObj => {
+      let newArray = this.state.data.filter(toy => toy.id !== toyObj.id)
+      newArray = [...newArray, toyObj]
+      this.setState({data: newArray.sort((a,b) => {return a.id - b.id})})
+    })
+  }
+
+  submitHandler = toyObj => {
+    console.log(toyObj)
+    let settings = {
+      method: 'POST',
+      header: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.state)
+    }
+    fetch('http://localhost:3000/toys', settings)
+    this.update()
+  }
+
+  render(){
+    return(
+    <>
+      { this.state.display
+        ?
+      <ToyForm submitHandler={this.submitHandler}/>
+        :
+      null
+    }
+    <div className="buttonContainer">
+      <button onClick={this.handleClick}> Add a Toy </button>
     </div>
-  );
+      <div id="toy-collection">
+        {this.state.data.map(toyObj => <ToyCard toy={toyObj} donateHandler={this.donateHandler} likeHandler={this.likeHandler}/>)}
+      </div>
+    </>
+    );
+  }
 }
 
 export default ToyContainer;
--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -2,21 +2,48 @@ import React, { Component } from 'react';
 
 class ToyForm extends Component {
 
+  state ={
+    name:"",
+    image:"",
+    likes: 0
+  }
+
+  changeHandler= e => {
+    this.setState({ [e.target.name]: e.target.value})
+  }
+
+  submitHandler = e => {
+    e.preventDefault()
+    this.postToys(this.state)
+    .then(resp => resp.json())
+    .then(data => this.props.submitHandler(data))
+  }
+
+  postToys = (toyObj) => {
+    const options = {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        "accept": "application/json"
+      },
+      body: JSON.stringify(toyObj)
+  }
+    return fetch("http://localhost:3000/toys", options)
+  }
+
   render() {
     return (
       <div className="container">
-        <form className="add-toy-form">
+        <form className="add-toy-form" onSubmit={this.submitHandler}>
           <h3>Create a toy!</h3>
-          <input type="text" name="name" placeholder="Enter a toy's name..." className="input-text"/>
+          <input type="text" name="name" placeholder="Enter a toy's name..." className="input-text" onChange={this.changeHandler} value={this.state.name}/>
           <br/>
-          <input type="text" name="image" placeholder="Enter a toy's image URL..." className="input-text"/>
+          <input type="text" name="image" placeholder="Enter a toy's image URL..." className="input-text" onChange={this.changeHandler} value={this.state.image}/>
           <br/>
           <input type="submit" name="submit" value="Create New Toy" className="submit"/>
         </form>
       </div>
-    );
+    )
   }
-
 }
-
 export default ToyForm;
